f151e3c61f55f8c8bec03db3351f7c307f00d438
Miksu Rankaviita <8172649+MiksuR@users.noreply.github.com>
2024-11-02 17:51:50 +0200

    Configure dmenu

--- home.nix
+++ home.nix
@@ -7,7 +7,26 @@
 
   home.packages = [
     (config.lib.nixGL.wrap pkgs.alacritty)
-    # # configuration. For example, this adds a command 'my-hello' to your
+    (pkgs.dmenu.overrideDerivation (oldAttrs: rec {
+      version = "5.2";
+      src = pkgs.fetchurl {
+        url = "https://dl.suckless.org/tools/dmenu-5.2.tar.gz";
+        sha256 = "sha256-1NTKd7WRQPJyJy21N+BbuRpZFPVoAmUtxX5hp3PUN5I=";
+      };
+      patches = [
+        (pkgs.fetchpatch {
+          url = "https://tools.suckless.org/dmenu/patches/border/dmenu-border-5.2.diff";
+          sha256 = "0kyi50z6c1y81gbq4kp9cnmg7gqpc9j17r7x227v6px68a8nj7p5";
+        })
+
+        (pkgs.fetchpatch {
+          url = "https://tools.suckless.org/dmenu/patches/center/dmenu-center-5.2.diff";
+          sha256 = "1jck88ypx83b73i0ys7f6mqikswgd2ab5q0dfvs327gsz11jqyws";
+        })
+      ];
+      configFile = pkgs.writeText "config.def.h" (builtins.readFile ./suckless/dmenu/config.def.h);
+    }))
+    # # For example, this adds a command 'my-hello' to your
     # # environment:
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
@@ -25,6 +44,7 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+    ".config/run-recent".source = ./suckless/dmenu/run-recent;
   };
 
   home.sessionVariables = {
